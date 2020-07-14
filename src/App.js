94cb1e2a30a258b57e8f42fc17aa01a2c1a21e38
Edward Ottorino <edward.ottorino@gmail.com>
2020-07-14 13:46:53 +1000

    'initial-commit'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Pokemon from './components/Pokemon'
+import Pokedex from './components/Pokedex'
+import { Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' render={props => <Pokedex {...props} />} />
+      <Route exact path='/:pokemonId' render={props => {
+        console.log('from app', props)
+        return (
+          <Pokemon {...props} />
+        )
+      }} />
+    </Switch>
   );
 }
+
 
 export default App;
---
+++ src/constans.js
@@ -0,0 +1,3 @@
+export const toFirstCharUppercase = a => (
+    a.charAt(0).toUpperCase() + a.slice(1)
+)
